06fcee14c9024fc877b5c6d1a5bcd26bdb332f57
Tom Dowdell <tom@lesarde.com>
2018-08-01 18:48:42 -0700

    Added some TODO: comments

--- Item.java
+++ Item.java
@@ -2,12 +2,12 @@ package project1;
 
 public class Item {
 ///Fields
-	String itemName;
+	String itemName; // TODO: This name is good; I generally don't repeat the enclosing type name, so would use "name".
 	String cost;
 
 
 
-	// TODO:Be consistent with field names and their associated constructor parameter names, e.g. either both itemName or name.
+	// TODO: Be consistent with field names and their associated constructor parameter names, e.g. either both cost or price.
 
 	////constructor 
 	public Item(String name, String price) {
