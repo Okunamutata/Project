8e9ecc6712f701f8989d63a887a066f07bbb7357
Tom Dowdell <tom@lesarde.com>
2018-08-01 18:40:40 -0700

    Tom test

--- Item.java
+++ Item.java
@@ -4,10 +4,11 @@ public class Item {
 ///Fields
 	String itemName;
 	String cost;
-	
-	
-	
-	
+
+
+
+	// TODO:Be consistent with field names and their associated constructor parameter names, e.g. either both itemName or name.
+
 	////constructor 
 	public Item(String name, String price) {
 		this.itemName = name;
